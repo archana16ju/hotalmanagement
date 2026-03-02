bfa95c82e606b0386c38c66e64cf41a62b1ef0c8
archana16ju <archana.work.in@gmail.com>
2026-03-02 17:10:52 +0530

    fix nextjs 15 params promise issue

--- src/app/table/[id]/page.tsx
+++ src/app/table/[id]/page.tsx
@@ -1,46 +1,20 @@
-type Props = {
-  params: { id: string }
-  searchParams: {
-    order?: string
-    payment?: string
-    review?: string
-  }
-}
+export default async function TablePage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
 
-export default function TablePage({ params, searchParams }: Props) {
-
-  const tableId = params.id
-
-  const orderEnabled = searchParams.order === '1'
-  const paymentEnabled = searchParams.payment === '1'
-  const reviewEnabled = searchParams.review === '1'
+  const { id } = await params
 
   return (
-    <div style={{ padding: 20 }}>
-
-      <h1>Welcome to Table {tableId}</h1>
+    <div>
+      <h1>Welcome to Table {id}</h1>
 
       <h3>Select option:</h3>
 
-      {orderEnabled && (
-        <>
-          <a href={`/order?table=${tableId}`}>Order</a>
-          <br/>
-        </>
-      )}
-
-      {paymentEnabled && (
-        <>
-          <a href={`/payment?table=${tableId}`}>Payment</a>
-          <br/>
-        </>
-      )}
-
-      {reviewEnabled && (
-        <>
-          <a href={`/review?table=${tableId}`}>Review</a>
-        </>
-      )}
+      <a href={`/order?table=${id}`}>Order</a><br/>
+      <a href={`/payment?table=${id}`}>Payment</a><br/>
+      <a href={`/review?table=${id}`}>Review</a>
 
     </div>
   )
