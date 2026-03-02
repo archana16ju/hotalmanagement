8f5b4b972f4b434a7d9a4dc01d05b5131d73f829
archana16ju <archana.work.in@gmail.com>
2026-03-02 16:48:03 +0530

    fixed dynamic table route

--- src/app/table/[id]/page.tsx
+++ src/app/table/[id]/page.tsx
@@ -1,16 +1,46 @@
-export default function TablePage({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string }
+  searchParams: {
+    order?: string
+    payment?: string
+    review?: string
+  }
+}
+
+export default function TablePage({ params, searchParams }: Props) {
 
   const tableId = params.id
 
+  const orderEnabled = searchParams.order === '1'
+  const paymentEnabled = searchParams.payment === '1'
+  const reviewEnabled = searchParams.review === '1'
+
   return (
-    <div>
+    <div style={{ padding: 20 }}>
+
       <h1>Welcome to Table {tableId}</h1>
 
       <h3>Select option:</h3>
-      
-      <a href={`/order?table=${tableId}`}>Order</a><br/>
-      <a href={`/payment?table=${tableId}`}>Payment</a><br/>
-      <a href={`/review?table=${tableId}`}>Review</a>
+
+      {orderEnabled && (
+        <>
+          <a href={`/order?table=${tableId}`}>Order</a>
+          <br/>
+        </>
+      )}
+
+      {paymentEnabled && (
+        <>
+          <a href={`/payment?table=${tableId}`}>Payment</a>
+          <br/>
+        </>
+      )}
+
+      {reviewEnabled && (
+        <>
+          <a href={`/review?table=${tableId}`}>Review</a>
+        </>
+      )}
 
     </div>
   )
