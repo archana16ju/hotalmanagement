38d62accc57b3b215848fe98060c3f1c8fef023a
archana16ju <archana.work.in@gmail.com>
2026-03-04 13:12:44 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: true, 
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
