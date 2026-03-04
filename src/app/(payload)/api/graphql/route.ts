ce332da173ba1396e1e5caea25e067534eb36678
archana16ju <archana.work.in@gmail.com>
2026-03-04 17:47:00 +0530

    Initial commit

--- src/app/(payload)/api/graphql/route.ts
+++ src/app/(payload)/api/graphql/route.ts
@@ -1,5 +1,8 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+export const runtime = 'nodejs'
+
 import config from '@payload-config'
 import { GRAPHQL_POST, REST_OPTIONS } from '@payloadcms/next/routes'
 
